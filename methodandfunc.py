24774fd26ddc9e576e9b01025d13471349613d2c
Aswanidevm <maswanidev@gmail.com>
2023-07-30 19:18:26 +0530

    clean

--- methodandfunc.py
+++ methodandfunc.py
@@ -12,3 +12,17 @@
 #methods
 # starts with '.''
 # methods are owned by the left of the "."
+
+# docstring
+
+'''
+can comment define for function
+'''
+def isodd(num):
+    # if num % 2 ==0:
+    #     return False
+    # else:
+    #     return True
+    return num % 2 ==0
+
+print(isodd(6))
