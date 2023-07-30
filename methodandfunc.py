0719b22333720b555dc5b7afe6a04e975becba8d
Aswanidevm <maswanidev@gmail.com>
2023-07-30 19:09:05 +0530

    #

---
+++ methodandfunc.py
@@ -0,0 +1,14 @@
+# methods vs function
+# lists()
+# print()
+# max()
+# min()
+# input()
+
+# def randomstuff():
+#     pass
+# randomstuff()
+
+#methods
+# starts with '.''
+# methods are owned by the left of the "."
