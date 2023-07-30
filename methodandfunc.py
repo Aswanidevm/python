d1103d54c77bb611d9156a0969eac0af2b87c10d
Aswanidevm <maswanidev@gmail.com>
2023-07-30 20:28:34 +0530

    highest

--- methodandfunc.py
+++ methodandfunc.py
@@ -1,28 +1,47 @@
-# methods vs function
-# lists()
-# print()
-# max()
-# min()
-# input()
+# # methods vs function
+# # lists()
+# # print()
+# # max()
+# # min()
+# # input()
 
-# def randomstuff():
-#     pass
-# randomstuff()
+# # def randomstuff():
+# #     pass
+# # randomstuff()
 
-#methods
-# starts with '.''
-# methods are owned by the left of the "."
+# #methods
+# # starts with '.''
+# # methods are owned by the left of the "."
 
-# docstring
+# # docstring
 
-'''
-can comment define for function
-'''
-def isodd(num):
-    # if num % 2 ==0:
-    #     return False
-    # else:
-    #     return True
-    return num % 2 ==0
+# '''
+# can comment define for function
+# '''
+# def isodd(num):
+#     # if num % 2 ==0:
+#     #     return False
+#     # else:
+#     #     return True
+#     return num % 2 ==0
 
-print(isodd(6))
+# print(isodd(6))
+
+# *args **kwargs(keyword arguments)
+
+# def highest_even(li):
+#     even = []
+#     for i in range(len(li)):
+#         if li[i] % 2 == 0:
+#             even.append(li[i])
+#     return max(even)
+# print(highest_even([10,2,3,4,8,12]))
+
+def highest_even(li):
+     even = []
+     for i in li:
+          if i % 2 == 0:
+               even.append(i)
+     return max(even)
+
+print(highest_even([10,2,3,4,8,12]))
