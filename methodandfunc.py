7e66d034fa207d878c469db978e76cfd7b1ea504
Aswanidevm <maswanidev@gmail.com>
2023-07-31 11:56:08 +0530

    high

--- methodandfunc.py
+++ methodandfunc.py
@@ -37,11 +37,23 @@
 #     return max(even)
 # print(highest_even([10,2,3,4,8,12]))
 
-def highest_even(li):
-     even = []
-     for i in li:
-          if i % 2 == 0:
-               even.append(i)
-     return max(even)
+# def highest_even(li):
+#      even = []
+#      for i in li:
+#           if i % 2 == 0:
+#                even.append(i)
+#      return max(even)
 
-print(highest_even([10,2,3,4,8,12]))
+# print(highest_even([10,2,3,4,8,12]))
+
+a= "helloooo0000o"
+if ((n := len(a)) > 10):
+    print(f"too long {n}")
+else:
+    print (" ok") 
+
+while (( n := len(a))>1):
+     print (n)
+     print(a)
+     a=a[:-1]
+
