5444f437c55ff2b46722cfacbae0a12007dc2666
Aswanidevm <maswanidev@gmail.com>
2023-08-08 10:54:49 +0530

    zsfdh

--- fourpillar.py
+++ fourpillar.py
@@ -4,6 +4,9 @@
 #polymorpism many forms
 
 class User():
+    # def __init__(self, email):
+    #     self.email = email
+    #     # print(email)
     def sign_in(self):
         print("Logged in")
     def attack():
@@ -11,6 +14,8 @@ class User():
 
 class Wizard(User):
     def __init__(self, name, power):
+        # User.__init__(self, email)
+        # super().__init__(email)
         self.name = name
         self.power = power
 
@@ -20,23 +25,46 @@ class Wizard(User):
 
 
 class Archer(User):
+    
     def __init__(self, name, num_arrow):
+        # User.__init__(self, email)
+        # super().__init__(email)
         self.name = name
         self.num_arrow = num_arrow
 
-    def attack(self):
+    def attacck(self):
         print(f"the power of Archer {self.name} is arrow with remaing {self.num_arrow}")
 
 
 wizard1 = Wizard("latha", "curse")
 archer1 = Archer("kedar", 30)
 
-print(wizard1.name)
-wizard1.sign_in()
-wizard1.attack()
+# print(wizard1.name)
+# wizard1.sign_in()
+# wizard1.attack()
 
-print(archer1.name)
-archer1.sign_in()
-archer1.attack()
+# print(archer1.name)
+# archer1.sign_in()
+# archer1.attack()
 
-print(isinstance(archer1, User))
+# print(isinstance(archer1, User))
+# print(archer1.email)
+def ataack(char):
+    print(f"Hello {char.name}")
+    char.sign_in()
+    char.attack()
+#     char.email
+    # print(f"Email id of {char.name} is {char.email}")
+
+# ataack(wizard1)
+# ataack(archer1)
+
+#multiple inheritance
+
+class Hybrid(Wizard, Archer):
+    def __init__(self, name, power, num_arrow):
+        Wizard.__init__(self, name, power)
+        Archer.__init__(self, name, num_arrow)
+
+hy = Hybrid("aswa", 30, 32)
+hy.attack()
